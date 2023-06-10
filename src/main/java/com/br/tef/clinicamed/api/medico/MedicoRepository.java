14044462c64ec20fb52c2bfa6062fba058bc100b
Thomas Erick <thominhaserick@gmail.com>
2023-06-10 16:46:46 -0300

    criado repository

---
+++ src/main/java/com/br/tef/clinicamed/api/medico/MedicoRepository.java
@@ -0,0 +1,6 @@
+package com.br.tef.clinicamed.api.medico;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MedicoRepository  extends JpaRepository<Medico, Long> {
+}
