0c36a9e8c673fa163d84f4e7cd8b53040e4ab61b
Thomas Erick <thominhaserick@gmail.com>
2023-06-10 16:46:36 -0300

    Criando entity

---
+++ src/main/java/com/br/tef/clinicamed/api/endereco/Endereco.java
@@ -0,0 +1,33 @@
+package com.br.tef.clinicamed.api.endereco;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Endereco {
+
+    private String logradouro;
+    private String bairro;
+    private String cep;
+    private String numero;
+    private String complemento;
+    private String cidade;
+    private String uf;
+
+    public Endereco(EnderecoDTO dto) {
+        this.logradouro = dto.logradouro();
+        this.bairro = dto.bairro();
+        this.cep = dto.cep();
+        this.uf = dto.uf();
+        this.cidade = dto.cidade();
+        this.numero = dto.numero();
+        this.complemento = dto.complemento();
+    }
+
+}
---
+++ src/main/java/com/br/tef/clinicamed/api/medico/Medico.java
@@ -0,0 +1,49 @@
+package com.br.tef.clinicamed.api.medico;
+
+import com.br.tef.clinicamed.api.endereco.Endereco;
+import com.br.tef.clinicamed.api.medico.enums.Especialidade;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "medicos")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Medico {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false, unique = true, nullable = false)
+    private Long id;
+    private String nome;
+    private String telefone;
+    private String email;
+    private String crm;
+    @Enumerated(EnumType.STRING)
+    private Especialidade especialidade;
+    @Embedded
+    private Endereco endereco;
+
+    public Medico(MedicoDTO dto) {
+        this.nome = dto.nome();
+        this.email = dto.email();
+        this.telefone = dto.telefone();
+        this.crm = dto.crm();
+        this.especialidade = dto.especialidade();
+        this.endereco = new Endereco(dto.endereco());
+    }
+
+}
