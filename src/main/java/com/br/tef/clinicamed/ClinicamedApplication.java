5493e7e8eed76c652e26e2b697da0ff7ce2f8976
Thomas Erick <thominhaserick@gmail.com>
2023-06-10 14:01:23 -0300

    Estrutura projeto Spring boot 3

---
+++ src/main/java/com/br/tef/clinicamed/ClinicamedApplication.java
@@ -0,0 +1,13 @@
+package com.br.tef.clinicamed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicamedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicamedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/tef/clinicamed/ClinicamedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.tef.clinicamed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicamedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
